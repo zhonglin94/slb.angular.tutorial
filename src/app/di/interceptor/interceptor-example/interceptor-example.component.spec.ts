49b39164d874a425503acde3213113ac36fc883a
ZYang25 <zyang25@slb.com>
2024-10-11 17:03:20 +0800

    add ut

--- src/app/di/interceptor/interceptor-example/interceptor-example.component.spec.ts
+++ src/app/di/interceptor/interceptor-example/interceptor-example.component.spec.ts
@@ -1,16 +1,37 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
 
-import { InterceptorExampleComponent } from './interceptor-example.component';
+import {InterceptorExampleComponent} from './interceptor-example.component';
+import {AbstractToastService} from "../../toast/abstract-toast.service";
+import {HttpClient} from "@angular/common/http";
+import {of} from "rxjs";
 
-describe('InterceptorExampleComponent', () => {
+const mockBook = {
+  title: 'book title'
+}
+fdescribe('InterceptorExampleComponent', () => {
   let component: InterceptorExampleComponent;
   let fixture: ComponentFixture<InterceptorExampleComponent>;
+  let abstractToastServiceSpy: jasmine.SpyObj<AbstractToastService>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ InterceptorExampleComponent ]
+    abstractToastServiceSpy = jasmine.createSpyObj('AbstractToastService', ['emit'])
+    let httpClientSpy = jasmine.createSpyObj('HttpClient', {
+      get: of(mockBook)
     })
-    .compileComponents();
+    await TestBed.configureTestingModule({
+      declarations: [InterceptorExampleComponent],
+      providers: [
+        {
+          provide: AbstractToastService,
+          useValue: abstractToastServiceSpy
+        },
+        {
+          provide: HttpClient,
+          useValue: httpClientSpy
+        }
+      ]
+    })
+      .compileComponents();
 
     fixture = TestBed.createComponent(InterceptorExampleComponent);
     component = fixture.componentInstance;
@@ -20,4 +41,10 @@ describe('InterceptorExampleComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should show the error toast', fakeAsync(() => {
+    component.onClick();
+    tick();
+    expect(abstractToastServiceSpy.emit).toHaveBeenCalled()
+  }))
 });
